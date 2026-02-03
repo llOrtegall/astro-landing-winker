2bddb2499d67923eae8185c1ea50aa5f01fa6381
llortegall <ivanortega_97@hotmail.com>
2026-02-03 13:42:18 -0500

    feat: add shared types for Vocal Remover demo

---
+++ src/components/VocalRemoverDemo/types.ts
@@ -0,0 +1,21 @@
+/**
+ * Tipos compartidos para el demo del Vocal Remover
+ */
+
+export interface Stem {
+  id: string;
+  name: string;
+  file: string;
+}
+
+export interface AudioPlayerState {
+  isPlaying: boolean;
+  currentTime: number;
+  duration: number;
+}
+
+export interface StemState {
+  id: string;
+  isMuted: boolean;
+  progress: number;
+}
